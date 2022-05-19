05e4a5b7b02051d31884438aa1c6e53b6ff0e53a
ibimongit <104595290+ibimongit@users.noreply.github.com>
2022-05-19 15:13:08 +0100

    Update Interactive_Dashboard_ML.py
    upadeted features:removed total combined crops

--- Interactive_Dashboard_ML.py
+++ Interactive_Dashboard_ML.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 from sklearn.model_selection import train_test_split
@@ -12,12 +12,35 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('Xdata.csv')
+
+feature_descriptions = {
+    'Yield_000_Tonnes':'total yield', 
+    'mean_temp':'mean temperature', 
+    'total_rain':'total rain', 
+    'Beans_and_peas':'beans and peas',
+    'Fodder_beet':'Fodder beet', 
+    'Kale_and_field_cabbage':'Kale and cabbage', 
+    'Oilseed_rape':'Oilseed rape', 
+    'Potatoes':'Potatoes',
+    'Spring_barley':'Spring barley', 
+    'Spring_oats':'Spring oats', 
+    'Spring_wheat':'Spring wheat', 
+    'Sugar_beet':'Sugar beet',
+    'Total_barley':'Total barley', 
+    'Total_oats':'Total oats', 
+    'Total_wheat':'Total wheat',
+    'Turnips':'Turnips', 
+    'Winter_barley':'Winter barley',
+    'Winter_oats':'Winter oats', 
+    'Winter_wheat':'Winter wheat',
+    'Crop_Cat_Id':'Crop ID'
+}
     
 X = data[['Yield_000_Tonnes', 'mean_temp', 'total_rain', 'Beans_and_peas',
        'Fodder_beet', 'Kale_and_field_cabbage', 'Oilseed_rape', 'Potatoes',
        'Spring_barley', 'Spring_oats', 'Spring_wheat', 'Sugar_beet',
        'Total_barley', 'Total_oats', 'Total_wheat',
-       'Total_wheat,_oats_and_barley', 'Turnips', 'Winter_barley',
+       'Turnips', 'Winter_barley',
        'Winter_oats', 'Winter_wheat']]
 y = data['Crop_Cat_Id']
 
@@ -26,7 +49,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8)
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-explainer = ClassifierExplainer(model, X_test, y_test)
+explainer = ClassifierExplainer(model, X_test, y_test,descriptions=feature_descriptions)
 ExplainerDashboard(explainer,mode='inline').run()
 
 
